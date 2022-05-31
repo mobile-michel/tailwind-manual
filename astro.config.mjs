e8cc06d5616b755525b31dfdfb299fff3e6ff71c
mobile-michel <info@maillard.li>
2022-05-31 21:50:13 +0200

    Intermediate commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,4 @@ export default {
     // hostname: 'localhost',  // The hostname to run the dev server on.
     // port: 3000,             // The port to run the dev server on.
   },
-  renderers: [
-    "@astrojs/renderer-preact"
-  ],
 };
